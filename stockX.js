88789b05ee68eb0379268a725f8957e8d50ea9db
JamesHopbourn <hopbourncontact@gmail.com>
2022-07-29 20:28:36 +0800

    stockX.js 初次上传
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

---
+++ stockX.js
@@ -0,0 +1,40 @@
+javascript: (function() {
+    function copyToClipboard(text) {
+        if (window.clipboardData && window.clipboardData.setData) {
+            return clipboardData.setData("Text", text);
+        } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+            var textarea = document.createElement("textarea");
+            textarea.textContent = text;
+            textarea.style.position = "fixed";
+            document.body.appendChild(textarea);
+            textarea.select();
+            try {
+                return document.execCommand("copy");
+            } catch (ex) {
+                console.warn("Copy to clipboard failed.", ex);
+                return false;
+            } finally {
+                document.body.removeChild(textarea);
+            }
+        }
+    }
+
+    function isJsonString(str) {
+        try {
+            JSON.parse(str);
+        } catch (e) {
+            return false;
+        }
+        return true;
+    }
+
+    var data = document.getElementsByTagName("script");
+    for (var i = 0; i < data.length; i++) {
+        if (isJsonString(data[i].innerText) && JSON.parse(data[i].innerText).hasOwnProperty('name')) {
+            if (JSON.parse(data[i].innerText)['name'].indexOf('StockX') == -1) {
+                product_id = document.getElementsByClassName('chakra-text css-1fo8xgy')[0].textContent;
+                copyToClipboard(`${JSON.parse(data[i].innerText)['name']} ${product_id}`.replace("'", ''));
+            }
+        }
+    }
+})();
