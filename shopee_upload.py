fc5d3df9013ef5d03639510744c2a193ac4ea491
JamesHopbourn <hopbourncontact@gmail.com>
2022-07-31 00:39:25 +0800

    网络异常处理 & 文件夹数字命名兼容
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- shopee_upload.py
+++ shopee_upload.py
@@ -81,6 +81,9 @@ def excel_item_index(item_name):
 # 获取图片名字
 def get_image_name(directory_name):
 	image_linklist = []
+	# 如果是纯数字的情况
+	if(type(directory_name) != type('JamesHopbourn')):
+		directory_name = str(int(directory_name))
 	files = os.listdir(directory_name)
 	for file in files:
 		if file.endswith(('.jpg', '.png', 'jpeg')):
@@ -89,14 +92,16 @@ def get_image_name(directory_name):
 
 # 上传商品图片
 def get_image_hash(image_path):
-	image_encode = open(image_path, 'rb').read().decode('latin-1')
-	request = requests.post("https://seller.shopee.cn/api/v3/general/upload_image/",
-		params={
-			"SPC_CDS": "d05d282f-3101-4864-b375-66621f6584b4",
-			"SPC_CDS_VER": "2",
-			"cnsc_shop_id": shopID,
-		},
-		data=f"""------WebKitFormBoundaryJBDkf5fKShesvHzh
+	try:
+		print(image_path)
+		image_encode = open(image_path, 'rb').read().decode('latin-1')
+		request = requests.post("https://seller.shopee.cn/api/v3/general/upload_image/",
+			params={
+				"SPC_CDS": "d05d282f-3101-4864-b375-66621f6584b4",
+				"SPC_CDS_VER": "2",
+				"cnsc_shop_id": shopID,
+			},
+			data=f"""------WebKitFormBoundaryJBDkf5fKShesvHzh
 Content-Disposition: form-data; name="file"; filename="blob"
 Content-Type: image/jpeg
 
@@ -104,28 +109,32 @@ Content-Type: image/jpeg
 ------WebKitFormBoundaryJBDkf5fKShesvHzh--
 
 """,
-		headers={
-			"Host": "seller.shopee.cn",
-			"accept": "application/json, text/plain, */*",
-			"accept-language": "zh-CN,zh;q=0.9",
-			"content-type": "multipart/form-data; boundary=----WebKitFormBoundaryJBDkf5fKShesvHzh",
-			"origin": "https://seller.shopee.cn",
-			"sc-fe-session": "9ea406c5-c0ec-45f9-aef5-c2aaf2d213f1",
-			"sc-fe-ver": "56878",
-			"sec-ch-ua": "\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"",
-			"sec-ch-ua-mobile": "?0",
-			"sec-ch-ua-platform": "\"macOS\"",
-			"sec-fetch-dest": "empty",
-			"sec-fetch-mode": "cors",
-			"sec-fetch-site": "same-origin",
-			"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
-		},
-		cookies=data_cookie,
-		auth=(),
-		verify=False
-	)
-	# 返回图片的 resource_id 列表
-	return json.loads(request.text)['data']['resource_id']
+			headers={
+				"Host": "seller.shopee.cn",
+				"accept": "application/json, text/plain, */*",
+				"accept-language": "zh-CN,zh;q=0.9",
+				"content-type": "multipart/form-data; boundary=----WebKitFormBoundaryJBDkf5fKShesvHzh",
+				"origin": "https://seller.shopee.cn",
+				"sc-fe-session": "9ea406c5-c0ec-45f9-aef5-c2aaf2d213f1",
+				"sc-fe-ver": "56878",
+				"sec-ch-ua": "\".Not/A)Brand\";v=\"99\", \"Google Chrome\";v=\"103\", \"Chromium\";v=\"103\"",
+				"sec-ch-ua-mobile": "?0",
+				"sec-ch-ua-platform": "\"macOS\"",
+				"sec-fetch-dest": "empty",
+				"sec-fetch-mode": "cors",
+				"sec-fetch-site": "same-origin",
+				"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
+			},
+			cookies=data_cookie,
+			auth=(),
+			verify=False
+		)
+		# 返回图片的 resource_id 列表
+		return json.loads(request.text)['data']['resource_id']
+	except (Exception):
+		print('网络异常')
+		network_status = 1
+		pass
 
 # 性别选项映射文件; 直接作用于 data
 def gender_option_mapper(gender_value):
@@ -244,27 +253,31 @@ def generate_request_data(excel_data, size_options, model_list, image_linklist):
 	return request_data
 
 def send_request(post_data):
-	response = requests.post(
-		url="https://seller.shopee.cn/api/v3/mtsku/create_mtsku/",
-		params={
-			"cnsc_shop_id": shopID,
-		},
-		headers={
-			"Cookie": f"{header_cookies}",
-			"Content-Type": "application/json; charset=utf-8",
-		},
-		data=json.dumps(post_data),
-		verify=False
-	)
-	status = json.loads(response.text)
-	if (status['code'] == -2) and ('mtsku.CreateMtskuRequest.Name' in status['message']):
-		print("商品名称有误，请修改后重试")
-	elif (status['code'] == 100010003 and ('mtsku title or description language is illegal ' in status['message'])):
-		print('商品名称或者描述有误')
-	else:
-		print("上架成功 ✅") if(status['code'] == 0) else print("失败")
-	print()
-	return status['code']
+	try:
+		response = requests.post(
+			url="https://seller.shopee.cn/api/v3/mtsku/create_mtsku/",
+			params={
+				"cnsc_shop_id": shopID,
+			},
+			headers={
+				"Cookie": f"{header_cookies}",
+				"Content-Type": "application/json; charset=utf-8",
+			},
+			data=json.dumps(post_data),
+			verify=False
+		)
+		status = json.loads(response.text)
+		if (status['code'] == -2) and ('mtsku.CreateMtskuRequest.Name' in status['message']):
+			print("商品名称有误，请修改后重试")
+		elif (status['code'] == 100010003 and ('mtsku title or description language is illegal ' in status['message'])):
+			print('商品名称或者描述有误')
+		else:
+			print("上架成功 ✅") if(status['code'] == 0) else print("失败")
+		print()
+		return status['code']
+	except (Exception):
+		network_status = 1
+		pass	
 
 def excel_launched_modify(launch_status):
 	table = xlrd.open_workbook(get_file_path('商品.xls'))
@@ -277,6 +290,7 @@ def excel_launched_modify(launch_status):
 	
 # 函数入口定义
 if __name__=="__main__":
+		network_status = 0
 		launch_status_array = []
 		data_cookie = parse_cookies()[0]
 		header_cookies = parse_cookies()[1]
@@ -286,6 +300,8 @@ if __name__=="__main__":
 		excel_duplicates_check(excel_sheet)
 		# 循环处理所有的产品
 		for index in range(len(excel_sheet)):
+			# 重置 network_status
+			network_status = 0
 			excel_data = excel_sheet[index]
 			# 如果标记为上传完成的产品就跳过
 			if(excel_data[excel_item_index('上架情况')].value == '完成'):
@@ -298,6 +314,8 @@ if __name__=="__main__":
 			image_linklist = []
 			for image in get_image_name(excel_data[excel_item_index('文件夹名')].value):
 				image_linklist.append(get_image_hash(image))
+			if network_status == 1:
+				continue
 			print(f"上传图片数量：{len(image_linklist)}")
 			# 获取价格和库存信息
 			excel_price = excel_data[excel_item_index('价格')].value
@@ -312,6 +330,8 @@ if __name__=="__main__":
 			post_data = generate_request_data(excel_data, size_options, model_list, image_linklist)
 			# 发送数据包上架商品返回状态码
 			launch_status_code = send_request(post_data)
+			if network_status == 1:
+				continue
 			# 根据状态码的情况决定是否追加
 			if (launch_status_code == 0):
 				launch_status_array.append(index)
