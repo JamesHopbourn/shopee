bc85d29db61d3117b400688e502359b6fc03c7e6
JamesHopbourn <hopbourncontact@gmail.com>
2022-08-23 18:05:24 +0800

    remove nothing use file
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- compile.bat
+++ compile.bat
@@ -1,14 +1,14 @@
 cd %USERPROFILE%\Downloads
 
-scp james@james.local:$HOME/code/shopee/shopee_upload.py ./
+scp james@james.local:$HOME/code/shopee/shopee_launch.py ./
 
 set /p version="input new version:"
 
 del shopee_upload.spec
 
-pyinstaller -F --add-data "api-ms-win-core-path-l1-1-0.dll;." --icon=shopee-logo.ico shopee_upload.py
+pyinstaller -F --add-data "api-ms-win-core-path-l1-1-0.dll;." --icon=shopee-logo.ico shopee_launch.py
 
-move /Y dist\shopee_upload.exe "%USERPROFILE%\Desktop\shopee %version%.exe"
+move /Y dist\shopee_launch.exe "%USERPROFILE%\Desktop\shopee %version%.exe"
 
 scp "%USERPROFILE%\Desktop\shopee %version%.exe" "james@james.local:~/Desktop"
 
