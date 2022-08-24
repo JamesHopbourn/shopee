2d91d2135999e3928e8c9619942fe4895c96b223
JamesHopbourn <hopbourncontact@gmail.com>
2022-08-24 10:41:41 +0800

    rmeove api ddl
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- compile.bat
+++ compile.bat
@@ -6,10 +6,10 @@ set /p version="input new version:"
 
 del shopee_upload.spec
 
-pyinstaller -F --add-data "api-ms-win-core-path-l1-1-0.dll;." --icon=shopee-logo.ico shopee_launch.py
+pyinstaller -F --icon=shopee-logo.ico shopee_launch.py
 
 move /Y dist\shopee_launch.exe "%USERPROFILE%\Desktop\shopee %version%.exe"
 
-scp "%USERPROFILE%\Desktop\shopee %version%.exe" "james@james.local:~/Desktop"
+scp "%USERPROFILE%\Desktop\shopee %version%.exe" "james@james.local:~/Code/shopee"
 
 exit
