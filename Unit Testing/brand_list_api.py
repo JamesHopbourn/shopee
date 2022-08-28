f4a43506d80fc1c4603a0abf3aa13034badb04e6
JamesHopbourn <hopbourncontact@gmail.com>
2022-08-28 15:05:28 +0800

    brand_list_api combine parse cookies
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- Unit Testing/brand_list_api.py
+++ Unit Testing/brand_list_api.py
@@ -1,17 +1,17 @@
 #!/usr/bin/env python3
-import requests
 import json
+import requests
+from urllib3 import *
+disable_warnings()
 
-brand_list = requests.get("https://seller.shopee.cn/api/v3/mtsku/get_mtsku_brand_list?category_ids=101298&brand_status=1&cursor=0&limit=100&cnsc_shop_id=821694597",
-	cookies={
-		"CNSC_SSO": "1ELepqUwZW2zcZz0NnQhkXyH3PYqOrMvDWxksH9VZ1EwiTROjE/0406+uBGd83js",
-		"SC_DFP": "PeohBwjkIhEoCMCUcF6iUGLClcD7hnES",
-		"SPC_CDS": "cf75bb61-df31-42f4-94d2-31f33c00315f",
-		"SPC_CNSC_TK": "2c4062420cb211edb29096a4f1819780",
-		"SPC_CNSC_UD": "1_1473107",
-		"_QPWSDCXHZQA": "c97622d9-c3de-4324-ded7-28fbf9d54818",
-		"fulfillment-language": "zh-CN"
-	},
+data_cookie = {}
+cookies = json.loads(open('./cookies.txt').read())
+for i in range(len(cookies)):
+	data = json.loads(json.dumps(cookies[i]))
+	data_cookie.update({data['name']: data['value']})
+
+brand_list = requests.get("https://seller.shopee.cn/api/v3/mtsku/get_mtsku_brand_list?category_ids=101298&brand_status=1&cursor=0&limit=100",
+	cookies=data_cookie,
 	auth=(),
 	verify=False
 )
