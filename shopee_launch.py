f71225ce3ecfbd2f4ca59e50197a2b3692401bbf
JamesHopbourn <hopbourncontact@gmail.com>
2022-08-30 20:04:42 +0800

    requests code format
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- shopee_launch.py
+++ shopee_launch.py
@@ -84,8 +84,8 @@ def get_brandID_list():
 		flag = True
 		while(flag):
 			brand_list = requests.get(f"https://seller.shopee.cn/api/v3/mtsku/get_mtsku_brand_list?brand_status=1&category_ids={key[0]}&cursor={key[1]}&limit=100",
-			cookies=data_cookie,
-			verify=False
+				cookies=data_cookie,
+				verify=False
 			)
 			data = json.loads(brand_list.text)['data']['list'][0]
 			flag = data.get('page_info')['has_next']
