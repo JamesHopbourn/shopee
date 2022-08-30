945e48d647f40996dbd941ca80cfdb3fc9adf557
JamesHopbourn <hopbourncontact@gmail.com>
2022-08-30 20:02:54 +0800

    item for in array
    Signed-off-by: JamesHopbourn <hopbourncontact@gmail.com>

--- Unit Testing/brand_list_api.py
+++ Unit Testing/brand_list_api.py
@@ -12,17 +12,16 @@ for i in range(len(cookies)):
 	data_cookie.update({data['name']: data['value']})
 
 brand = {}
-cousor = [[101298, 0],[100089, 0]]
-for i in range(len(cousor)):
+for key in [[101298, 0],[100089, 0]]:
 	flag = True
 	while(flag):
-		brand_list = requests.get(f"https://seller.shopee.cn/api/v3/mtsku/get_mtsku_brand_list?brand_status=1&category_ids={cousor[i][0]}&cursor={cousor[i][1]}&limit=100",
+		brand_list = requests.get(f"https://seller.shopee.cn/api/v3/mtsku/get_mtsku_brand_list?brand_status=1&category_ids={key[0]}&cursor={key[1]}&limit=100",
 		cookies=data_cookie,
 		verify=False
 		)
 		data = json.loads(brand_list.text)['data']['list'][0]
 		flag = data.get('page_info')['has_next']
-		cousor[i][1] = data.get('page_info')['cursor']
+		key[1] = data.get('page_info')['cursor']
 		for item in data['brand_list']:
 			brand.update({item['name'].lower(): item['brand_id']})
 print(brand)
